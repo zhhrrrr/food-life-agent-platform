281ad7edf73f65162b1ab50aa81f221b7b943c12
Alex7 <905591339@qq.com>
2026-08-29 11:04:06 +0800

    feat: release redis stock on seckill refund

--- food-trade-service/food-trade-domain/src/main/java/com/foodlife/trade/domain/order/seckill/refund/strategy/PaidSeckillRefundStrategy.java
+++ food-trade-service/food-trade-domain/src/main/java/com/foodlife/trade/domain/order/seckill/refund/strategy/PaidSeckillRefundStrategy.java
@@ -5,18 +5,24 @@ import com.foodlife.trade.domain.order.model.OrderRefundBehaviorEntity;
 import com.foodlife.trade.domain.order.model.OrderRefundCommandEntity;
 import com.foodlife.trade.domain.order.seckill.model.SeckillOrderEntity;
 import com.foodlife.trade.domain.order.seckill.repository.ISeckillRepository;
+import com.foodlife.trade.domain.order.seckill.repository.ISeckillStockRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaidSeckillRefundStrategy extends AbstractSeckillRefundStrategy {
 
-    public PaidSeckillRefundStrategy(ISeckillRepository seckillRepository) {
+    private final ISeckillStockRepository seckillStockRepository;
+
+    public PaidSeckillRefundStrategy(ISeckillRepository seckillRepository,
+                                     ISeckillStockRepository seckillStockRepository) {
         super(seckillRepository);
+        this.seckillStockRepository = seckillStockRepository;
     }
 
     @Override
     public OrderRefundBehaviorEntity refundOrder(OrderRefundCommandEntity command, DiningOrderEntity order) {
         SeckillOrderEntity seckillOrder = seckillRepository.refundPaidSeckillOrder(order);
+        seckillStockRepository.releaseActivityStock(seckillOrder.getActivityId(), order.getUserId());
         return buildSuccessBehavior(command, order, seckillOrder);
     }
 }
